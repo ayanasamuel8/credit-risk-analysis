a6e226376cca98e22ad28c82f7b9906eb75db9b8
Ayana Samuel <ayanasamuel8@gmailcom>
2025-07-01 22:22:51 +0300

    fix: updated docker-compose for production and flake8 issue

--- src/api/main.py
+++ src/api/main.py
@@ -14,7 +14,10 @@ def root():
 @app.post("/predict", response_model=PredictionResponse)
 def predict_risk(tx: Transaction):
     import mlflow
-    mlflow.set_tracking_uri("file:///C:/Users/user/Documents/Datasience/credict-risk-analysis/notebooks/mlruns")
+    mlflow.set_tracking_uri(
+        "file:///C:/Users/user/Documents/Datasience/" +
+        "credict-risk-analysis/notebooks/mlruns"
+    )
     data = pd.DataFrame([tx.dict()])
     prediction = predict(data)
     return PredictionResponse(
