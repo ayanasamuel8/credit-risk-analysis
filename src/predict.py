0c1923e568ca36e36d575ee1a3acbf5c24626e03
Ayana Samuel <ayanasamuel8@gmailcom>
2025-07-01 18:35:58 +0300

    new: prediction added

--- src/predict.py
+++ src/predict.py
@@ -1,5 +1,24 @@
-"""Prediction script for credit risk model."""
+import mlflow.pyfunc
 import pandas as pd
-def predict(input_data: pd.DataFrame, model: object):
-    # Load model and make predictions
-    pass
+from src.data_processing import create_feature_pipeline
+import joblib
+
+def predict(df: pd.DataFrame, model_name: str ="CreditRisk_xgboost_model", stage: str ="Production"):
+    model_uri = f"models:/{model_name}/{stage}"
+    model = mlflow.pyfunc.load_model(model_uri)
+
+    data = df.copy()
+    pipeline = joblib.load("../artifacts/fitted_pipeline.pkl")
+    transformed_data = pipeline.transform(data)
+    # 🏷️ Build full column list
+    cat_cols = pipeline.named_steps["preprocessor"].named_transformers_["cat"]\
+        .named_steps["onehot"].get_feature_names_out(['ProductCategory', 'ChannelId', 'PricingStrategy'])
+
+    all_cols = ['Amount', 'Value', 'Frequency', 'AvgAmount', 'AmountStdDev', 'Recency'] + list(cat_cols)
+
+    # 🧾 Create transformed DataFrame
+    transformed_df = pd.DataFrame(transformed_data, columns=all_cols)
+    predictions = model.predict(transformed_df)
+    data["is_high_risk"] = predictions
+
+    return data
